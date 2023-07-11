32674d431f4a182b0b295bbcc9fec5edade497b6
lucas-project <niki.grey.uk@gmail.com>
2023-07-10 22:14:18 -0400

    added lab4

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -101,19 +101,17 @@ class Enrollment(models.Model):
     # Has a grade point for each question
     # Has question content
     # Other fields and methods you would like to design
-#class Question(models.Model):
+class Question(models.Model):
+
     # Foreign key to lesson
     # question text
     # question grade/mark
-class Question(models.Model):
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='questions')
-    text = models.TextField()
-    grade_point = models.IntegerField()
+    lesson = models.ForeignKey(Course, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200)
+    grade = models.IntegerField(default=1)
 
-    def is_get_score(self, selected_choice_ids):
-        correct_choices = self.choices.filter(is_correct=True)
-        correct_choice_ids = set(choice.id for choice in correct_choices)
-        return correct_choice_ids == set(selected_choice_ids)
+    def __str__(self):
+        return self.question_text  
 
     # <HINT> A sample model method to calculate if learner get the score of the question
     def is_get_score(self, selected_ids):
@@ -131,11 +129,13 @@ class Question(models.Model):
     # Choice content
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
-# class Choice(models.Model):
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='choices')
-    text = models.TextField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
     is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.choice_text
 
 # <HINT> The submission model
 # One enrollment could have multiple submission
